b076f9bf3528914428e49d94fb299f7bb75833e4
guocong <congguocn@outlook.com>
2023-03-20 11:53:33 -0400

    Initial commit

---
+++ backend.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = "080fbc8d9a8b8bae72823dbadcb91666"
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+# if __name__=="__main__":
+#     print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
